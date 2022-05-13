04d0afa121fe25db7ea3c61de3cb0d60bc299266
sildater <41552783+sildater@users.noreply.github.com>
2022-05-13 12:39:35 +0200

    synth with note cc

--- synth_cc.rb
+++ synth_cc.rb
@@ -1,14 +1,26 @@
 use_synth :prophet
 
 s = nil
+
+ccdict = { 40 => 64,
+           41 => 48 }
 val = 80
+pitch = 48
+release = 0.5
 
 live_loop :SYN do
-  s = play 48, sustain: 0.1, release: 0.1, attack: 0.01, decay: 0.01, cutoff: val
+  s = play ccdict[41],
+    sustain: 0.1,
+    release: 0.1,
+    attack: 0.01,
+    decay: 0.01,
+    cutoff: ccdict[40]
   sleep 0.125
 end
 
+
 live_loop :cc do
   sleep 0.01
-  ccno, val = sync "/midi:js2sonicpi_2:1/control_change"
+  ccno, val = sync "/midi:js2sonicpi_0:1/control_change"
+  ccdict[ccno] = val
 end
